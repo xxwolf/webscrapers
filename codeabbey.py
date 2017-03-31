789b29fdc81527a36f40fcc9dbdd72900085ebf2
Volodymyr Matskiv <xxwolfsilver@gmail.com>
2017-04-01 00:44:10 +0300

    next  commit

--- codeabbey.py
+++ codeabbey.py
@@ -3,12 +3,17 @@ import urllib.request
 
 with urllib.request.urlopen('http://www.codeabbey.com/index/user_ranking') as response:
     html = response.read()
+# r'((\<tr class="centered none"\>).*?(\<\/tr\>))'
 
+# m = re.match(r'(?P<start>\<tr class="centered none"\>)(?P<inner>.*?)(?P<finish>\<\/tr\>)', html.decode("utf-8"), )
+# print (m)
+
+rows = []
 i = 0
-for td in re.finditer(r"<tr></tr>", html.decode("utf-8")):
+for tr in re.finditer(r'\<tr class="centered none"\>.*?(\<\/tr\>)', html.decode("utf-8"), flags=re.DOTALL):
     i += 1
-    print(i, ' ', td)
-
-print(html)
-
-# hello
+    # print (i)
+    r = tr.group(0)
+    rows.append(r)
+    print(rows)
+    #print (i, ' -> ',tr.group(0))
