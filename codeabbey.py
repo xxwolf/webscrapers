fd9dc9a45fded9fadc002798492b06d96e06996c
Volodymyr Matskiv <xxwolfsilver@gmail.com>
2017-04-01 03:14:00 +0300

    next  commit

--- codeabbey.py
+++ codeabbey.py
@@ -5,6 +5,7 @@ with urllib.request.urlopen('http://www.codeabbey.com/index/user_ranking') as re
     html = response.read()
 
 rows = ['    Num', '    Lang', '    Rank', '    Enli', '    Shit', '    Tasks', 'Errr']
+
 i = 0
 for tr in re.finditer(r'(\<tr class="centered none"\>)(.*?)(\<\/tr\>)', html.decode("utf-8"), flags=re.DOTALL):
     i += 1
@@ -18,8 +19,9 @@ for tr in re.finditer(r'(\<tr class="centered none"\>)(.*?)(\<\/tr\>)', html.dec
         print(td_it, rows[td_it], d)
         td_it += 1
         if td_it == 2:
-            for span in re.finditer(r'(\<span\>)(.*?)(\<\/span\>)', d):
-                print(span.group(2))
+            for span in re.finditer(r'(\<span class="rank rank(.*?)\>)(.*?)(\<\/span\>)', r):
+                print('Eeee>', span.group(3))
+                continue
                 # rows.append(r)
                 # print(rows)
                 # print (i, ' -> ',tr.group(0))
